a0179b09a893056e4e4cd62750230c0a8b91ec9a
faditotah <faditotah02@gmail.com>
2024-07-09 15:28:01 +0300

    Add files via upload
    Task1: Bitmap Binarization

---
+++ Task1/Task1/Filters.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Task1
+{
+    public class BitmapFilter
+    {
+        private static Bitmap Convert24BitTo8Bit(Bitmap b)
+        {
+            Bitmap bit8 = new Bitmap(b.Width, b.Height, PixelFormat.Format8bppIndexed);          // Create 8bit Bitmap for new Image
+
+            ColorPalette palette = bit8.Palette;                                                 // Set color palette of new image to gray scale
+            for (int i = 0; i < 256; i++)
+            {
+                palette.Entries[i] = Color.FromArgb(i, i, i);
+            }
+            bit8.Palette = palette;
+
+            BitmapData bmpData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),          // Lock input bitmap data into memory
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            BitmapData newBmpData = bit8.LockBits(new Rectangle(0, 0, b.Width, b.Height),   // Lock output bitmap data into memory
+                ImageLockMode.WriteOnly,
+                PixelFormat.Format8bppIndexed);
+
+            IntPtr Scan0 = bmpData.Scan0;                                                       // Retrieve address for first pixel data
+            IntPtr Scan1 = newBmpData.Scan0;
+            int height = b.Height; int width = b.Width;                                         // Assign bMap parameters to integer variables
+            int nOffset8 = newBmpData.Stride - bit8.Width;                                      // Calculate offset for 8bit output
+            int nOffset24 = bmpData.Stride - b.Width * 3;                                       // Calculate offset for 24bit input
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;                                                  // Cast scan0 to byte pointers
+                byte* pNew = (byte*)(void*)Scan1;
+                
+                for (int y = 0; y < height; y++)                                                // Iterate over the pixels of the input image
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        pNew[0] = (byte)(0.299 * p[2] + 0.587 * p[1] + 0.114 * p[0]);          // Set the value of each pixel (byte) of the 8bit image to the
+                                                                                               // corresponding grayscale value from the 24bit image
+                        p += 3;                                                                // Increment to the next 24bit pixel
+                        ++pNew;                                                                // Iterate to the next 8bit pixel
+                    }
+                    p += nOffset24;                                                            // Forgo offset at end of bitmap width 
+                    pNew += nOffset8;
+                }
+            }
+            b.UnlockBits(bmpData);                                                             // Unlock bitmap data from memory
+            bit8.UnlockBits(newBmpData);
+            return bit8;                                                                       // Return 8bit image
+        } 
+        public static int Mean(Bitmap b)                                                       // Method calculates mean intensity of 8bit grayscale images
+        {
+            if (b.PixelFormat == PixelFormat.Format24bppRgb)                                   // If input is in 24bit, use previous method to convert to 8bit
+            {
+                b = Convert24BitTo8Bit(b);
+            }
+            else if (b.PixelFormat == PixelFormat.Format8bppIndexed)                           // Ensures 8bit format is acceptable
+            {
+            }
+            else
+            {                                                                                 // Only 24bit or 8bit inputs are acceptable
+                throw new ArgumentException("Image must be 24bit or 8bit grayscale");
+            }
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height),        // Locks bitmap data into memory
+                ImageLockMode.ReadOnly, PixelFormat.Format8bppIndexed);
+            IntPtr scan0 = bmData.Scan0;                                                      // Retrieves address of first pixel data
+            int nOffset = bmData.Stride - b.Width;                                            // Assigns offset to integer vairable
+            int height = b.Height;                                                            // Assign bitmap width and height to variables
+            int width = b.Width;
+            int sum = 0;                                                                      // Initialize sum to zero
+            int numPix = width * height;                                                      // Number of pixels in bitmap
+            unsafe
+            {
+                byte* p = (byte*)(void*)scan0;                                                // Cast address to byte pointer
+                for (int y = 0; y < height; ++y)
+                {
+                    for (int x = 0; x < width; ++x)                                           // Iterate over pixels in each row and add intensity value to running sum
+                    {
+                        sum += p[0];
+                        ++p;
+                    }                                      
+                    p += nOffset;                   
+                }
+            }
+            b.UnlockBits(bmData);
+            int meanInt = sum / numPix;                                                       // Calculate intensity using summed intensity values and number of pixels
+            return meanInt;                                                                   // Return mean value
+        }
+        public static Bitmap Threshold(Bitmap b, int nVal)                                    // Binarization method using threshold nVal
+        {
+            Bitmap newImage;                                                                  // Initialize output bitmap
+            if (b.PixelFormat == PixelFormat.Format8bppIndexed)
+            {
+                newImage = (Bitmap)b.Clone();                                                 // If input is 8bit, then output is a manipulated version of input
+            }
+            else if (b.PixelFormat == PixelFormat.Format24bppRgb)
+            {
+                newImage = Convert24BitTo8Bit(b);                                             // If input is 24bit, then output bitmap is a manipulated version of converted input
+            }
+            else
+            {
+                throw new ArgumentException("Image must be 24bit or 8bit grayscale");
+            }
+            BitmapData bmData = newImage.LockBits(new Rectangle(0, 0, newImage.Width,     // Lock bitmap data into memory
+                newImage.Height), ImageLockMode.ReadWrite, newImage.PixelFormat);
+                
+            IntPtr Scan2 = bmData.Scan0;                                                      // Obtain address of first pixel data
+            int nOffset = bmData.Stride - newImage.Width;                                     // Assign offset to integer variable
+            int height = newImage.Height; int width = newImage.Width;                         // Assign bitmap width and height to integers
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan2;                                                // Cast address to byte pointer
+                for (int y = 0; y < height; ++y)                                              // Iterate over pixels in bitmap
+                {
+                    for (int x = 0; x < width; ++x)
+                    {
+                        p[0] = (p[0] >= nVal) ? (byte)255 : (byte)0;                          // Check value of each byte (pixel), then assign it to either black or white
+                        ++p;                                                                  // Increment to next byte
+                    }
+                    p += nOffset;                                                             // Skip offset at end of bitmap row
+                }
+            }
+            newImage.UnlockBits(bmData);                                                      // Unlock bitmap data from memory
+            return newImage;                                                                  // Return binarized imag
+        }
+        
+        
+    }
+}
---
+++ Task1/Task1/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Task1                         
+{
+    public class Form1 : Form
+    {
+        private Bitmap m_Bitmap;
+        private Bitmap m_Undo;
+        private MainMenu mainMenu1;
+        private MenuItem menuItem1;
+        private MenuItem menuItem3;
+        private MenuItem menuItem2;
+        private MenuItem FileLoad;
+        private MenuItem FileSave;
+        private MenuItem FileExit;
+        private MenuItem MeanThresh;
+        private MenuItem StaticThresh;      
+        private MenuItem Undo;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
+
+        private Form1()
+        {
+            InitializeComponent();
+            m_Undo = new Bitmap(2, 2);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.FileLoad = new System.Windows.Forms.MenuItem();
+            this.FileSave = new System.Windows.Forms.MenuItem();
+            this.FileExit = new System.Windows.Forms.MenuItem();
+            this.Undo = new System.Windows.Forms.MenuItem();
+            this.MeanThresh = new System.Windows.Forms.MenuItem();
+            this.StaticThresh = new System.Windows.Forms.MenuItem();
+
+            //MAin Menu
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+                                                                                      this.menuItem1,
+                                                                                      this.menuItem3,
+                                                                                      this.menuItem2});
+            //menu item 1
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+                                                                                      this.FileLoad,
+                                                                                      this.FileSave,
+                                                                                      this.FileExit});
+            this.menuItem1.Text = "File";
+
+            //FileLoad
+            this.FileLoad.Index = 0;
+            this.FileLoad.Shortcut = System.Windows.Forms.Shortcut.CtrlL;
+            this.FileLoad.Text = "Load";
+            this.FileLoad.Click += new System.EventHandler(this.File_Load);
+
+            //FileSave
+            this.FileSave.Index = 1;
+            this.FileSave.Shortcut = System.Windows.Forms.Shortcut.CtrlS;
+            this.FileSave.Text = "Save";
+            this.FileSave.Click += new System.EventHandler(this.File_Save);
+            // 
+            // FileExit
+            // 
+            this.FileExit.Index = 2;
+            this.FileExit.Text = "Exit";
+            this.FileExit.Click += new System.EventHandler(this.File_Exit);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Index = 1;
+            this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+                                                                                      this.Undo});
+            this.menuItem2.Text = "Edit";
+            // 
+            // Undo
+            // 
+            this.Undo.Index = 0;
+            this.Undo.Text = "Undo";
+            this.Undo.Click += new System.EventHandler(this.OnUndo);
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Index = 2;
+            this.menuItem3.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+                                                                                      this.MeanThresh, 
+                                                                                      this.StaticThresh});
+            this.menuItem3.Text = "Binarize";
+            // 
+            // BW Mean
+            // 
+            this.MeanThresh.Index = 0;
+            this.MeanThresh.Text = "Mean Binarization";
+            this.MeanThresh.Click += new System.EventHandler(this.Filter_MeanThresh);
+            //
+            // BW Static
+            // 
+            this.StaticThresh.Index = 1;
+            this.StaticThresh.Text = "Static Binarization";
+            this.StaticThresh.Click += new System.EventHandler(this.Filter_StaticThresh);
+
+
+            // Form1
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(488, 421);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Static & Mean Binarization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+    
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (m_Bitmap != null)
+            {
+                Graphics g = e.Graphics;
+                g.DrawImage(m_Bitmap, new Rectangle(this.AutoScrollPosition.X, this.AutoScrollPosition.Y, (int)(m_Bitmap.Width), (int)(m_Bitmap.Height)));
+            }
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+        }
+
+        private void File_Load(object sender, System.EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/*.jpg)|*.bmp/*.jpg";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (DialogResult.OK == openFileDialog.ShowDialog())
+            {
+                m_Bitmap = (Bitmap)Bitmap.FromFile(openFileDialog.FileName, false);
+                this.AutoScroll = true;
+                this.AutoScrollMinSize = new Size((int)(m_Bitmap.Width), (int)(m_Bitmap.Height));
+                this.Invalidate();
+            }
+        }
+
+        private void File_Save(object sender, System.EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/*.jpg)|*.bmp/*.jpg";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (DialogResult.OK == saveFileDialog.ShowDialog())
+            {
+                m_Bitmap.Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void File_Exit(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void Filter_MeanThresh(object sender, System.EventArgs e)
+        {
+            m_Undo = (Bitmap)m_Bitmap.Clone();
+            m_Bitmap = BitmapFilter.Threshold(m_Bitmap, BitmapFilter.Mean(m_Bitmap)); // Apply horizontal concatenation
+            Invalidate(); // Redraw the form
+        }
+        private void Filter_StaticThresh(object sender, System.EventArgs e)
+        {
+            Threshold dlg = new Threshold();
+            dlg.nValue = 0;
+
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                m_Undo = (Bitmap)m_Bitmap.Clone();
+                m_Bitmap = BitmapFilter.Threshold(m_Bitmap, dlg.nValue);
+                Invalidate();
+            }
+        }
+
+        private void OnUndo(object sender, System.EventArgs e)
+        {
+            Bitmap temp = (Bitmap)m_Bitmap.Clone();
+            m_Bitmap = (Bitmap)m_Undo.Clone();
+            m_Undo = (Bitmap)temp.Clone();
+            this.Invalidate();
+        }
+    }
+}
